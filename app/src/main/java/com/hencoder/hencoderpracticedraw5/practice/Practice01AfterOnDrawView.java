13ec46757dc8bd90697ad89efc9190994ab0bd0b
Rain Liu <liuwenbin168@126.com>
2018-05-24 14:03:24 +0800

    Check in...

--- app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice01AfterOnDrawView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice01AfterOnDrawView.java
@@ -3,12 +3,14 @@ package com.hencoder.hencoderpracticedraw5.practice;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import com.hencoder.hencoderpracticedraw5.R;
 
@@ -40,13 +42,25 @@ public class Practice01AfterOnDrawView extends AppCompatImageView {
         // 由于这期的重点是绘制代码的位置而不是绘制代码本身，所以直接给出绘制代码，你只要解除注释就好
         // 爽吧？
 
-        /*Drawable drawable = getDrawable();
+        int width = getWidth();
+        width = canvas.getWidth();
+        Drawable drawable = getDrawable();
+        width = drawable.getIntrinsicWidth();
         if (drawable != null) {
             canvas.save();
-            canvas.concat(getImageMatrix());
+            Matrix matrix = getImageMatrix();
+            matrix.preRotate(45);
+            matrix.preScale(1.3f, 1.3f);
+            canvas.concat(matrix);
+            width = canvas.getWidth();
+            Log.e("HenCoder", "width == " + width);
+
             Rect bounds = drawable.getBounds();
             canvas.drawText(getResources().getString(R.string.image_size, bounds.width(), bounds.height()), 20, 40, paint);
             canvas.restore();
-        }*/
+
+            this.setScaleType(ScaleType.MATRIX);
+            this.setImageMatrix(matrix);
+        }
     }
 }
